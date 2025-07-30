a7fb5365161b823331346cd7b9fe8a4535df1409
noleforte <noleforte@gmail.com>
2025-07-30 14:29:44 +0200

    Update CORS configuration in server.js to include the new domain for enhanced accessibility, ensuring proper access for the frontend application.

--- aquin-main WEB/chat-ai-back-main/server.js
+++ aquin-main WEB/chat-ai-back-main/server.js
@@ -14,7 +14,12 @@ const app = express();
 const PORT = 4000;
 const TOKEN = process.env.TOKEN;
 
-const allowedOrigins = ['https://aquin.vercel.app', 'https://aquin.xyz', 'https://loono.boo', '*'];
+const allowedOrigins = [
+   'https://aquin.vercel.app',
+   'https://aquin.xyz',
+   'https://loono.boo',
+   'https://ely-lemon.vercel.app' // ← добавляем твой новый домен
+ ];
 
 app.use(
    cors({
